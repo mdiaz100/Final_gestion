2f72e91a828f7b2770e1b1903602284939b96f51
mdiaz100 <124297233+mdiaz100@users.noreply.github.com>
2024-11-15 04:34:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,26 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-firebase_config = os.getenv("FIREBASE_CONFIG")
-if firebase_config:
-    cred = credentials.Certificate(json.loads(firebase_config))
-    firebase_admin.initialize_app(cred)
+# Construir el diccionario de configuración de Firebase desde las variables de entorno
+firebase_config = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),  # Asegúrate de reemplazar los saltos de línea en la clave privada
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
 
+# Inicializar Firebase con las credenciales
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred)
+
+# Conectar a Firestore
 db = firestore.client()
 
 app = Flask(__name__)
